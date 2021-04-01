811c43a8bb139eaa057f288b0f9af3e26d62ca3a
ricbatera <67552275+ricbatera@users.noreply.github.com>
2021-04-01 00:08:03 -0300

    add tela de loading na listagem de saidas

--- src/app/formularios/listagem-saidas/listagem-saidas.component.ts
+++ src/app/formularios/listagem-saidas/listagem-saidas.component.ts
@@ -24,7 +24,7 @@ export class ListagemSaidasComponent implements OnInit {
   // controles de visibilidade
   pago: boolean = true
   naoPago: boolean = true
-  loading: boolean = false;
+  loading: boolean = true;
 
   responsaveisList: Responsavel[];
   listaSaidas = [];
