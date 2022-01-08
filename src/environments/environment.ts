eccefd3dab71f2f89651d539a39cb94c47d4436c
Ricardo Alves Roberto <ricardo.roberto@rdrtech.com.br>
2022-01-08 12:47:38 -0300

    alterado path do backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   logRequest: true,
   traceRequest: true,
-  // URLSERVIDOR: 'http://localhost:8080/',
-  URLSERVIDOR: 'https://contasbackend.herokuapp.com/'
+  URLSERVIDOR: 'http://localhost:8080/',
+  // URLSERVIDOR: 'https://contasbackend.herokuapp.com/'
 };
 
 /*
